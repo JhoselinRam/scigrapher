ad659b0d9530ed5e81685923b77599dcd6635006
= <=>
2023-03-01 13:42:15 -0600

    Station 01/03/23 1

--- src/Data/Data_Types.ts
+++ src/Data/Data_Types.ts
@@ -1,5 +1,6 @@
 import { Axis_Property, Graph2D_State } from "../Graph2D/Graph2D_Types";
 import { Line_Chart, Line_Chart_Options, Line_Chart_State } from "./LineChart/LineChart_Types";
+import { Vector_Field, Vector_Field_Options, Vector_Field_State } from "./VectorField/Vector_Field_Types";
 
 export type Datasets = "linechart";
 
@@ -10,9 +11,9 @@ export interface Data_Object_State {
     dirtify : (sort?:boolean)=>void
 }
 
-export type Dataset_Types = Line_Chart
-export type Dataset_States = Line_Chart_State
-export type Dataset_Options = Line_Chart_Options;
+export type Dataset_Types = Line_Chart | Vector_Field;
+export type Dataset_States = Line_Chart_State | Vector_Field_State;
+export type Dataset_Options = Line_Chart_Options | Vector_Field_Options;
 
 export interface Data_General_Generator<T extends Dataset_Types, P extends Dataset_States> {
     dataHandler : T,
@@ -28,3 +29,7 @@ export interface Data_General<T extends Dataset_Types> {
 }
 
 export type Draw_Data_Callback = (state : Graph2D_State)=>void;
+
+export type Field_Data<T> = Array<Array<T>>;
+
+export type Field_Property_Generator<T> = T | Field_Data<T> | (()=>Field_Data<T>)
